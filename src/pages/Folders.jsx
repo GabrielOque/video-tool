05434a200e758d6fff0612e0ac652fe3e8edba56
Gabriel Oquendo <oquendogabriel18@gamil.com>
2024-03-19 23:38:31 -0500

    pages

---
+++ src/pages/Folders.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Input from "../components/Input";
+import TableFolderRow from "../components/TableFolderRow";
+import data from "../mock/mock.json";
+const Folders = () => {
+  const [inputvalue, setInputValue] = useState("");
+  return (
+    <div className="h-full w-[83%] bg-white rounded-xl p-10">
+      <Header />
+      <Input
+        placeholder={"Buscar carpeta"}
+        setInputValue={setInputValue}
+        inputValue={inputvalue}
+      />
+      <table className="mt-10 w-full table-fixed">
+        <thead>
+          <tr>
+            <td className="px-4 py-2">
+              <input type="checkbox" />
+            </td>
+            <th className="w-3/12 px-4 py-2 text-left">Nombre</th>
+            <th className="w-2/12 px-4 py-2 text-right">Videos</th>
+            <th className="w-3/12 px-4 py-2 text-right">Tamaño</th>
+            <th className="w-3/12 px-4 py-2 text-right">Última modificación</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.folders
+            .filter((folder) =>
+              folder.name
+                .toLocaleLowerCase()
+                .includes(inputvalue.toLocaleLowerCase())
+            )
+            .map((folder) => (
+              <TableFolderRow
+                key={folder.id}
+                row={folder}
+                icon={"fa-regular fa-folder text-primary mr-2 text-xl"}
+              />
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Folders;
---
+++ src/pages/Videos.jsx
@@ -0,0 +1,63 @@
+import TableFolderRow from "../components/TableFolderRow";
+import Input from "../components/Input";
+import data from "../mock/mock.json";
+import { useParams, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const Videos = () => {
+  const [inputvalue, setInputValue] = useState("");
+  const { name } = useParams();
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/");
+  };
+  return (
+    <div className="h-full w-[83%] bg-white rounded-xl p-10">
+      <div className="text-2xl flex h-[52px]">
+        <i
+          className="fa-solid fa-arrow-left bg-primary text-center p-2 text-white rounded-lg mr-2 h-[40px] cursor-pointer"
+          onClick={handleNavigate}
+        />
+        <p>
+          Home
+          <span className="font-bold"> / {name}</span>
+        </p>
+      </div>
+      <Input
+        placeholder={"Buscar video"}
+        setInputValue={setInputValue}
+        inputValue={inputvalue}
+      />
+      <table className="mt-10 w-full table-fixed">
+        <thead>
+          <tr>
+            <td className="px-4 py-2">
+              <input type="checkbox" />
+            </td>
+            <th className="w-3/12 px-4 py-2 text-left">Nombre</th>
+            <th className="w-2/12 px-4 py-2 text-right">Tamaño</th>
+            <th className="w-3/12 px-4 py-2 text-right">Duración</th>
+            <th className="w-3/12 px-4 py-2 text-right">Última modificación</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.videos
+            .filter((video) =>
+              video.name
+                .toLocaleLowerCase()
+                .includes(inputvalue.toLocaleLowerCase())
+            )
+            .map((folder) => (
+              <TableFolderRow
+                key={folder.id}
+                row={folder}
+                icon={"fa-regular fa-circle-play text-primary mr-2 text-xl"}
+              />
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Videos;
